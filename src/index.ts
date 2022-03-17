1482c3ec6783b3c9fec5c333c7f970fe8b035d80
Alve Svarén <alve@hotmail.se>
2022-03-17 22:34:12 +0100

    Add websocket test

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import Koa from "koa";
 import KoaRouter from "@koa/router";
 import koaStatic from "koa-static";
 import koaLogger from 'koa-logger';
+import koaWs from 'koa-easy-ws';
+import WebSocket from "ws";
 
 const config = {
     port: 3000,
@@ -14,10 +16,21 @@ router.get("/", (ctx, next) => {
     ctx.body = {online: true};
 });
 
+router.get('/ws', async (ctx, next) => {
+    if (ctx.ws) {
+        const ws: WebSocket = await ctx.ws();
+        ws.on("message", msg => {
+            console.log(msg);
+        })
+    }
+})
+
+
 app.use(koaLogger());
+app.use(koaStatic("static"));
+app.use(koaWs());
 app.use(router.routes());
 app.use(router.allowedMethods());
-app.use(koaStatic("static"));
 
 app.listen(config.port, () => {
     console.log(`Server running on port ${config.port}`);
